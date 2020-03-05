cbab54ba3507b2a848413a56bd74b88865b84886
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-03-04 18:35:21 -0600

    [ADD] homework javascript

---
+++ p8helloKoders/js/main.js
@@ -0,0 +1,24 @@
+// console.log("Hello Koders!!!");
+// console.warn("Hello Koders!!!");
+// console.error("Hello Koders!!!");
+
+// var string = "Una cadena de caracteres";
+// var number = 4;
+// var object = {
+//   name: "Fernanda",
+//   age: 23,
+//   phone: "3445675467"
+// };
+// var array = ["koder 1", "koder 2", "koder 3"];
+
+// var arr = Array();
+
+// console.log(arr);
+
+var x = 150;
+var y = 10;
+
+console.log(`La suma de ${x} y ${y} es igual a `, x + y);
+console.log(`La resta de ${x} y ${y} es igual a `, x - y);
+console.log(`La multiplicación de ${x} y ${y} es igual a `, x * y);
+console.log(`La división de ${x} y ${y} es igual a `, x / y);
