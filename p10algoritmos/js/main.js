e02741c7ad3c95df589171c2914a23da864bf6ce
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-03-05 17:56:03 -0600

    [ADD] Practice javascript

--- p10algoritmos/js/main.js
+++ p10algoritmos/js/main.js
@@ -57,7 +57,6 @@ side2 = parseInt(side2);
 side3 = parseInt(side3);
 
 semiPerimeter = (side1 + side2 + side3)/2;
-console.log('Perimeter: ', semiPerimeter);
 
 areaResult = (semiPerimeter)*((semiPerimeter - side1)*(semiPerimeter - side2)*(semiPerimeter - side3));
 areaResult = Math.sqrt(areaResult);
