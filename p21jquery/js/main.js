04c9a7e5503b57c9c28e6de7a3a34a64de966e7f
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-04-06 14:23:29 -0500

    [ADD] practice - 21

--- p21jquery/js/main.js
+++ p21jquery/js/main.js
@@ -2,17 +2,14 @@ var kodersArray = [];
 
 
 const deleteKoder = (event) => {
+    // $(event.targer).data("index-koder")
     let index = event.target.dataset.indexKoder;
-    // console.log(index)
-    // console.log(kodersArray)
-    // $(`tr#${index}`).remove()
-    // deleteInArray(index);
     kodersArray.splice(index, 1)
     showInTable()
 }
 
 const showInTable = () => {
-    $('#koder-wrapper').empty();
+    $('#koders-table').find('tbody').empty();
     kodersArray.forEach((koder, index) => {
         let {name, phone, email} = koder;
         let newKoder = `
@@ -21,14 +18,12 @@ const showInTable = () => {
             <td>${name}</td>
             <td>${phone}</td>
             <td>${email}</td>
-            <td><div class="btn btn-danger delete-koder" id=koder-${index} data-index-koder=${index}>Eliminar</div></td>
+            <td><div class="btn btn-danger delete-koder" data-index-koder=${index}>Eliminar</div></td>
         </tr>
         `
-        $('#koder-wrapper').append(newKoder);
-        $(`#koder-${index}`).on('click', deleteKoder);
-        // $(`.delete-koder`).on('click', deleteKoder);
-
+        $('#koders-table').find('tbody').append(newKoder);
     })
+    $(`.delete-koder`).on('click', deleteKoder);
 }
 
 const cleanInputs = () => {
@@ -52,3 +47,32 @@ const getData = () => {
 }
 
 $('#btn-send').on('click', getData);
+
+/* AJAX */
+
+const getKodersFromDB = () => {
+    kodersArray = []
+    $.ajax({
+        url:"https://javascript-ajax-d0ce6.firebaseio.com/fernanda/koders/.json",
+        method: "GET",
+        success: (response) => {
+            // console.log(response)
+            $.each(response, (key, value)=>{
+                kodersArray.push({...value, key})
+            })
+            showInTable()
+        }
+    })
+}
+
+const deleteKoderDB = (event)=> {
+    let koderKey = $(event.targer).data("index-koder")
+    $.ajax({
+        url:`https://javascript-ajax-d0ce6.firebaseio.com/fernanda/koders/${koderKey}.json`,
+        method: 'DELETE',
+        success: (response) => {
+            console.log(response)
+            getKodersFromDB()
+        }
+    })
+}
